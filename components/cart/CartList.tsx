b1c8c861addd06476b6110abcb3ba74c3f708427
xxalanbritoxx <kevin9abel5@gmail.com>
2023-12-06 19:27:11 -0700

    Cart update

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,78 +1,83 @@
-import { initialData } from "@/database/products";
-import {
-  Box,
-  Button,
-  CardActionArea,
-  CardMedia,
-  Grid,
-  Link,
-  Typography,
-} from "@mui/material";
-import NextLink from "next/link";
-import { ItemCounter } from "../ui";
-import { FC, useContext } from "react";
-import { CartContext } from "@/context/cart";
+import { FC, useContext } from 'react';
+import NextLink from 'next/link';
+import { Box, Button, CardActionArea, CardMedia, Grid, Link, Typography } from '@mui/material';
+
+import { ItemCounter } from '../ui';
+import { CartContext } from '@/context/cart/CartContext';
+import { ICartProduct } from '@/interfaces';
+
 
 interface Props {
-  editable?: boolean;
+    editable?: boolean;
 }
 
-export const CartList: FC<Props> = ({ editable }) => {
-  const { cart } = useContext(CartContext);
+export const CartList: FC<Props> = ({ editable = false }) => {
 
-  return (
-    <>
-      {cart.map((product) => (
-        <Grid container spacing={2} key={product.slug} sx={{ mb: 1 }}>
-          <Grid item xs={3}>
-            {/* TODO LLEVAR A LA PAGINA DEL PRODUCTO */}
-            <NextLink href={`/product/${ product.slug }`} passHref>
-              <Link>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    image={`/products/${product.image}`}
-                    sx={{ borderRadius: "5px" }}
-                  />
-                </CardActionArea>
-              </Link>
-            </NextLink>
-          </Grid>
-          <Grid item xs={7}>
-            <Box display="flex" flexDirection="column">
-              <Typography variant="body1">{product.title}</Typography>
-              <Typography variant="body1">
-                Size: <strong>M</strong>{" "}
-              </Typography>
-              {/* Conditional */}
-              {editable ? (
-                <ItemCounter
-                  currentValue={ product.quantity }
-                  maxValue={ 10 }
-                  updatedQuantity={ () => {} }
-                />
-              ) : (
-                <Typography variant="h5">{product.quantity} { product.quantity > 1 ? 'items' : 'items' }</Typography>
-              )}
-            </Box>
-          </Grid>
-          <Grid
-            item
-            xs={2}
-            display="flex"
-            alignItems="center"
-            flexDirection="column"
-          >
-            <Typography variant="subtitle1"> {`$${product.price}`} </Typography>
-            {/* Editable */}
-            {editable && (
-              <Button color="secondary" variant="text">
-                Remove
-              </Button>
-            )}
-          </Grid>
-        </Grid>
-      ))}
-    </>
-  );
-};
+    const { cart, updateCartQuantity, removeCartProduct } = useContext(CartContext);
+
+    const onNewCartQuantityValue = (product: ICartProduct, newQuantityValue: number) => {
+        product.quantity = newQuantityValue;
+        updateCartQuantity( product );
+    }
+
+    return (
+        <>
+            {
+                cart.map( product => (
+                    <Grid container spacing={2} key={ product.slug + product.size } sx={{ mb:1 }}>
+                        <Grid item xs={3}>
+                            {/* TODO: llevar a la página del producto */}
+                            <NextLink href={`/product/${ product.slug }`} passHref>
+                                <Link>
+                                    <CardActionArea>
+                                        <CardMedia 
+                                            image={ `/products/${ product.image }` }
+                                            component='img'
+                                            sx={{ borderRadius: '5px' }}
+                                        />
+                                    </CardActionArea>
+                                </Link>
+                            </NextLink>
+                        </Grid>
+                        <Grid item xs={7}>
+                            <Box display='flex' flexDirection='column'>
+                                <Typography variant='body1'>{ product.title }</Typography>
+                                <Typography variant='body1'>Size: <strong>{ product.size }</strong></Typography>
+
+                                {
+                                    editable 
+                                    ? (
+                                        <ItemCounter 
+                                            currentValue={ product.quantity }
+                                            maxValue={ 10 } 
+                                            updatedQuantity={ ( value ) => onNewCartQuantityValue(product, value )}
+                                        />
+                                    )
+                                    : (
+                                        <Typography variant='h5'>{ product.quantity } { product.quantity > 1 ? 'productos':'producto' }</Typography>
+                                    )
+                                }
+                                
+                            </Box>
+                        </Grid>
+                        <Grid item xs={2} display='flex' alignItems='center' flexDirection='column'>
+                            <Typography variant='subtitle1'>{ `$${ product.price }` }</Typography>
+                            
+                            {
+                                editable && (
+                                    <Button 
+                                        variant='text' 
+                                        color='secondary' 
+                                        onClick={ () => removeCartProduct( product ) }
+                                    >
+                                        Remove
+                                    </Button>
+                                )
+                            }
+                        </Grid>
+                    </Grid>
+                ))
+            }
+        </>
+    )
+}
--- components/cart/OrderSummary.tsx
+++ components/cart/OrderSummary.tsx
@@ -1,34 +1,42 @@
-import { Grid, Typography } from "@mui/material"
+import { useContext } from 'react';
+import { Grid, Typography } from '@mui/material';
+import { CartContext } from '../../context/cart/CartContext';
+import { currency } from '../../utils';
+
 
 export const OrderSummary = () => {
+
+    const { numberOfItems, subTotal, total, tax } = useContext( CartContext );
+    
   return (
     <Grid container>
+        
         <Grid item xs={6}>
             <Typography>No. Products</Typography>
         </Grid>
         <Grid item xs={6} display='flex' justifyContent='end'>
-            <Typography>3 items</Typography>
+            <Typography>{numberOfItems} { numberOfItems > 1 ? 'products': 'product' }</Typography>
         </Grid>
 
         <Grid item xs={6}>
             <Typography>SubTotal</Typography>
         </Grid>
         <Grid item xs={6} display='flex' justifyContent='end'>
-            <Typography>{` $${155.6} `}</Typography>
+            <Typography>{ currency.format(subTotal) }</Typography>
         </Grid>
 
         <Grid item xs={6}>
-            <Typography>Tax (15%)</Typography>
+            <Typography>Taxes ({ Number(process.env.NEXT_PUBLIC_TAX_RATE) * 100 }%)</Typography>
         </Grid>
         <Grid item xs={6} display='flex' justifyContent='end'>
-            <Typography>{` $${35.34} `}</Typography>
+            <Typography>{ currency.format(tax) }</Typography>
         </Grid>
 
         <Grid item xs={6} sx={{ mt:2 }}>
             <Typography variant="subtitle1">Total:</Typography>
         </Grid>
         <Grid item xs={6} sx={{ mt:2 }} display='flex' justifyContent='end'>
-            <Typography variant="subtitle1">{` $${190.94} `}</Typography>
+            <Typography variant="subtitle1">{ currency.format(total) }</Typography>
         </Grid>
 
     </Grid>
